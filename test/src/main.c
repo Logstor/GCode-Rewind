73732a833535775f920cdd299d513304ce243232
Logstor <a.rottger_rydahl@live.dk>
2021-02-22 18:45:43 +0100

    Added CUnit for testing.

--- test/src/main.c
+++ test/src/main.c
@@ -1,7 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <CUnit/CUnit.h>
+
 #include "../../src/gcodeRewind.h"
 
 int main(int argc, char** argv)
 {
-    GCodeRevert(NULL, NULL);
+    // Start Framework
+    if (CU_initialize_registry() != CUE_SUCCESS)
+    {
+        puts("ERROR! Couldn't allocate memory for test registry!");
+        return -1;
+    }
+
+    // Create Suite
+    
+    
+    // Clean Framework
+    CU_cleanup_registry();
     return 0;
 }
